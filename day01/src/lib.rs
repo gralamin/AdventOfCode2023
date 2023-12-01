55dd69aa7f1c3d1f2bdd6ff4b4e2881aa8669c4b
Glen Nelson <glen.nelson.1@gmail.com>
2023-11-30 22:18:16 -0700

    Day 1: Add answer to part 1
    This is a simple iteration through once to get the first char, and once to get the
    last char. We use find / rfind to do this. We then simply map them together using
    zip and do some multiplication. Due to the input, we don't need signed numbers so
    no conversion from the "to_digit" is required.

--- day01/src/lib.rs
+++ day01/src/lib.rs
@@ -1,18 +1,16 @@
 extern crate filelib;
 
-pub use filelib::load;
-pub use filelib::split_lines_by_blanks;
+pub use filelib::load_no_blanks;
 
-/// Get the highest sum of a group of vectors
+/// Get the sum of all first and last numbers in each line. If a single number appears in a line, count it for both.
 /// ```
-/// let vec1 = vec![vec![1000, 2000, 3000], vec![4000], vec![5000, 6000], vec![7000,8000,9000], vec![10000]];
-/// assert_eq!(day01::puzzle_a(&vec1), 24000);
+/// let vec1: Vec<String> = vec!["1abc2", "pqr3stu8vwx", "a1b2c3d4e5f", "treb7uchet"].iter().map(|s| s.to_string()).collect();
+/// assert_eq!(day01::puzzle_a(&vec1), 142);
 /// ```
-/// This has to be in here, due to how rust doctests work...
-pub fn puzzle_a(calorie_lists: &Vec<Vec<i32>>) -> i32 {
-    let mut sums = to_sums(calorie_lists);
-    sums.sort();
-    return sums.pop().unwrap();
+pub fn puzzle_a(string_list: &Vec<String>) -> u32 {
+    let tens: Vec<u32> = string_list.iter().map(|x| x.chars().nth(x.find(char::is_numeric).unwrap()).unwrap().to_digit(10).unwrap()).collect();
+    let ones: Vec<u32> = string_list.iter().map(|x| x.chars().nth(x.rfind(char::is_numeric).unwrap()).unwrap().to_digit(10).unwrap()).collect();
+    return tens.iter().zip(ones.iter()).map(|(x, y)| x * 10 + y).sum();
 }
 
 fn to_sums(calorie_list: &Vec<Vec<i32>>) -> Vec<i32> {
@@ -24,7 +22,6 @@ fn to_sums(calorie_list: &Vec<Vec<i32>>) -> Vec<i32> {
 /// let vec1 = vec![vec![1000, 2000, 3000], vec![4000], vec![5000, 6000], vec![7000,8000,9000], vec![10000]];
 /// assert_eq!(day01::puzzle_b(&vec1), 45000);
 /// ```
-/// This has to be in here, due to how rust doctests work...
 pub fn puzzle_b(calorie_lists: &Vec<Vec<i32>>) -> i32 {
     let mut sums = to_sums(calorie_lists);
     sums.sort();
