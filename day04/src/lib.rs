145046298a40c09653041be09d73842cc0345860
Glen Nelson <glen.nelson.1@gmail.com>
2023-12-02 23:08:21 -0700

    day04: Added template

---
+++ day04/src/lib.rs
@@ -0,0 +1,25 @@
+extern crate filelib;
+
+pub use filelib::load_no_blanks;
+
+/// Foo
+/// ```
+/// let vec1: Vec<String> = vec![
+///     "foo"
+/// ].iter().map(|s| s.to_string()).collect();
+/// assert_eq!(day04::puzzle_a(&vec1), 0);
+/// ```
+pub fn puzzle_a(string_list: &Vec<String>) -> u32 {
+    return 0;
+}
+
+/// Foo
+/// ```
+/// let vec1: Vec<String> = vec![
+///     "foo"
+/// ].iter().map(|s| s.to_string()).collect();
+/// assert_eq!(day04::puzzle_b(&vec1), 0);
+/// ```
+pub fn puzzle_b(string_list: &Vec<String>) -> u32 {
+    return 0;
+}
