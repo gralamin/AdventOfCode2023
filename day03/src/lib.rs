373ef5b9db22736f4bac3f4e92b20d71125c2dd3
Glen Nelson <glen.nelson.1@gmail.com>
2023-12-02 22:36:21 -0700

    day03: Answer to part 1
    We simply create a grid and traverse it, keeping track of state as we go.
    Nothing really clever here, just simple logic. Some optimization to only
    check for symbols when we need to.

--- day03/src/lib.rs
+++ day03/src/lib.rs
@@ -1,16 +1,162 @@
 extern crate filelib;
+extern crate gridlib;
 
+use crate::gridlib::GridTraversable;
 pub use filelib::load_no_blanks;
 
-/// Foo
+type PartNumber = u32;
+
+#[derive(PartialEq, Debug, Copy, Clone)]
+enum ParsedSpace {
+    Blank,
+    Numeral(PartNumber),
+    Symbol(char),
+}
+
+fn parse_grid(string_list: &Vec<String>) -> gridlib::Grid<ParsedSpace> {
+    let width = string_list.iter().nth(0).unwrap().len();
+    let height = string_list.len();
+    let mut values: Vec<ParsedSpace> = vec![];
+    for s in string_list {
+        let parsed_line: Vec<ParsedSpace> = s
+            .chars()
+            .map(|c| match c {
+                '.' => ParsedSpace::Blank,
+                '0' => ParsedSpace::Numeral(0),
+                '1' => ParsedSpace::Numeral(1),
+                '2' => ParsedSpace::Numeral(2),
+                '3' => ParsedSpace::Numeral(3),
+                '4' => ParsedSpace::Numeral(4),
+                '5' => ParsedSpace::Numeral(5),
+                '6' => ParsedSpace::Numeral(6),
+                '7' => ParsedSpace::Numeral(7),
+                '8' => ParsedSpace::Numeral(8),
+                '9' => ParsedSpace::Numeral(9),
+                _ => ParsedSpace::Symbol(c),
+            })
+            .collect();
+        values.extend(parsed_line);
+    }
+
+    return gridlib::Grid::new(width, height, values);
+}
+
+fn get_parts_adjacent_to_symbols(grid: &gridlib::Grid<ParsedSpace>) -> Vec<PartNumber> {
+    let mut result: Vec<PartNumber> = vec![];
+    let mut cur_number: Option<PartNumber> = None;
+    let mut cur_y = 0;
+    let mut cur_part_num = false;
+    for coord in grid.coord_iter() {
+        if coord.y != cur_y {
+            // We switched rows
+            // First if there is a number we should add, do so
+            if let Some(n) = cur_number {
+                if cur_part_num {
+                    result.push(n);
+                }
+            }
+            // set back to defaults with the new y.
+            cur_y = coord.y;
+            cur_number = None;
+            cur_part_num = false;
+        }
+        let v = grid.get_value(coord).unwrap();
+        match v {
+            ParsedSpace::Blank => {
+                if cur_part_num {
+                    // we aren't on a number anymore
+                    if let Some(n) = cur_number {
+                        if cur_part_num {
+                            result.push(n);
+                        }
+                    }
+                    cur_part_num = false;
+                }
+                // cur_number should always be reset
+                cur_number = None;
+            }
+            ParsedSpace::Numeral(new_ones_place) => {
+                if let Some(x) = cur_number {
+                    cur_number = Some(x * 10 + new_ones_place);
+                } else {
+                    cur_number = Some(new_ones_place);
+                }
+            }
+            ParsedSpace::Symbol(_) => {
+                if cur_part_num {
+                    // we aren't on a number anymore
+                    if let Some(n) = cur_number {
+                        if cur_part_num {
+                            result.push(n);
+                        }
+                    }
+                    cur_part_num = false;
+                }
+                // cur_number should always be reset
+                cur_number = None;
+            }
+        }
+        if let Some(_) = cur_number {
+            if !cur_part_num {
+                // Check if we should be a part number
+                for possible_loc in grid.get_adjacent_coordinates(coord) {
+                    if cur_part_num {
+                        break;
+                    }
+                    let z = grid.get_value(possible_loc).unwrap();
+                    match z {
+                        ParsedSpace::Symbol(_) => {
+                            cur_part_num = true;
+                        }
+                        _ => {}
+                    }
+                }
+                for possible_loc in grid.get_diag_adjacent_coordinates(coord) {
+                    if cur_part_num {
+                        break;
+                    }
+                    let z = grid.get_value(possible_loc).unwrap();
+                    match z {
+                        ParsedSpace::Symbol(_) => {
+                            cur_part_num = true;
+                        }
+                        _ => {}
+                    }
+                }
+            }
+        }
+    }
+
+    // Check if we have a last value, if so add it
+    if let Some(n) = cur_number {
+        if cur_part_num {
+            result.push(n);
+        }
+    }
+
+    return result;
+}
+
+/// Add up all parts on the grid that are adjacent to a symbol thats not a period.
 /// ```
 /// let vec1: Vec<String> = vec![
-///     "foo"
+///    "467..114..",
+///    "...*......",
+///    "..35..633.",
+///    "......#...",
+///    "617*......",
+///    ".....+.58.",
+///    "..592.....",
+///    "......755.",
+///    "...$.*....",
+///    ".664.598..",
 /// ].iter().map(|s| s.to_string()).collect();
-/// assert_eq!(day02::puzzle_a(&vec1), 0);
+/// assert_eq!(day03::puzzle_a(&vec1), 4361);
 /// ```
-pub fn puzzle_b(string_list: &Vec<String>) -> u32 {
-    return 0;
+pub fn puzzle_a(string_list: &Vec<String>) -> PartNumber {
+    let grid = parse_grid(string_list);
+    let part_nums = get_parts_adjacent_to_symbols(&grid);
+    return part_nums.iter().sum();
 }
 
 /// Foo
@@ -18,8 +164,8 @@ pub fn puzzle_b(string_list: &Vec<String>) -> u32 {
 /// let vec1: Vec<String> = vec![
 ///     "foo"
 /// ].iter().map(|s| s.to_string()).collect();
-/// assert_eq!(day02::puzzle_b(&vec1), 0);
+/// assert_eq!(day03::puzzle_b(&vec1), 0);
 /// ```
-pub fn puzzle_b(string_list: &Vec<String>) -> u32 {
+pub fn puzzle_b(string_list: &Vec<String>) -> PartNumber {
     return 0;
 }
