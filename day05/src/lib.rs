a78705921399196a3b4f22230a38cf93b9951a58
Glen Nelson <glen.nelson.1@gmail.com>
2023-12-04 23:12:51 -0700

    day05: Part 1 solution
    This is dumb, its just a ton of parsing, and then applying some offsets.
    
    There must be a better way to do this.
    
    That said, this did teach me a bunch about Rust's type system, so it was
    good for learning.

--- day05/src/lib.rs
+++ day05/src/lib.rs
@@ -1,40 +1,288 @@
 extern crate filelib;
 
-pub use filelib::load_no_blanks;
+pub use filelib::load;
+pub use filelib::split_lines_by_blanks;
 
-/// Foo
-/// ```
-/// let vec1: Vec<String> = vec![
-///     "foo"
-/// ].iter().map(|s| s.to_string()).collect();
-/// assert_eq!(day05::puzzle_a(&vec1), 0);
-/// ```
-pub fn puzzle_a(string_list: &Vec<String>) -> u32 {
-    return 0;
+type IdNum = u64;
+
+pub fn example_input() -> Vec<Vec<String>> {
+    return vec![
+        vec!["seeds: 79 14 55 13"]
+            .iter()
+            .map(|s| s.to_string())
+            .collect(),
+        vec!["seed-to-soil map:", "50 98 2", "52 50 48"]
+            .iter()
+            .map(|s| s.to_string())
+            .collect(),
+        vec!["soil-to-fertilizer map:", "0 15 37", "37 52 2", "39 0 15"]
+            .iter()
+            .map(|s| s.to_string())
+            .collect(),
+        vec![
+            "fertilizer-to-water map:",
+            "49 53 8",
+            "0 11 42",
+            "42 0 7",
+            "57 7 4",
+        ]
+        .iter()
+        .map(|s| s.to_string())
+        .collect(),
+        vec!["water-to-light map:", "88 18 7", "18 25 70"]
+            .iter()
+            .map(|s| s.to_string())
+            .collect(),
+        vec![
+            "light-to-temperature map:",
+            "45 77 23",
+            "81 45 19",
+            "68 64 13",
+        ]
+        .iter()
+        .map(|s| s.to_string())
+        .collect(),
+        vec!["temperature-to-humidity map:", "0 69 1", "1 0 69"]
+            .iter()
+            .map(|s| s.to_string())
+            .collect(),
+        vec!["humidity-to-location map:", "60 56 37", "56 93 4"]
+            .iter()
+            .map(|s| s.to_string())
+            .collect(),
+    ];
 }
 
-/// Foo
-/// ```
-/// let vec1: Vec<String> = vec![
-///     "foo"
-/// ].iter().map(|s| s.to_string()).collect();
-/// assert_eq!(day05::puzzle_b(&vec1), 0);
-/// ```
-pub fn puzzle_b(string_list: &Vec<String>) -> u32 {
-    return 0;
+#[derive(PartialEq, Debug, Copy, Clone)]
+enum AlmanacItem {
+    Seed,
+    Soil,
+    Fertilizer,
+    Water,
+    Light,
+    Temperature,
+    Humidity,
+    Location,
 }
 
-/// Delete this after starting on puzzle_a.
-/// ```
-/// let vec1: Vec<u32> = vec![];
-/// let vec2: Vec<u32> = vec![1];
-/// assert_eq!(day05::coverage_workaround(&vec1), 1);
-/// assert_eq!(day05::coverage_workaround(&vec2), 2);
-/// ```
-pub fn coverage_workaround(a: &Vec<u32>) -> u32 {
-    if a.len() == 0 {
-        return 1;
-    } else {
-        return 2;
+#[derive(PartialEq, Debug, Clone)]
+struct RangeMap {
+    src: AlmanacItem,
+    dst: AlmanacItem,
+    src_start_ids: Vec<IdNum>,
+    src_end_ids: Vec<IdNum>,
+    dst_start_ids: Vec<IdNum>,
+    dst_end_ids: Vec<IdNum>,
+}
+
+impl RangeMap {
+    pub fn new(
+        src_starts: Vec<IdNum>,
+        dst_starts: Vec<IdNum>,
+        range_size: Vec<IdNum>,
+        src_type: AlmanacItem,
+        dst_type: AlmanacItem,
+    ) -> RangeMap {
+        let src_ends = src_starts
+            .iter()
+            .zip(range_size.clone())
+            .map(|(src_id, cur_range)| src_id + cur_range)
+            .collect();
+        let dst_ends = dst_starts
+            .iter()
+            .zip(range_size)
+            .map(|(dst_id, cur_range)| dst_id + cur_range)
+            .collect();
+        return RangeMap {
+            src: src_type,
+            dst: dst_type,
+            src_start_ids: src_starts.clone(),
+            src_end_ids: src_ends,
+            dst_start_ids: dst_starts.clone(),
+            dst_end_ids: dst_ends,
+        };
     }
+
+    fn convert_id(&self, id: IdNum) -> IdNum {
+        for (index, (start_id, end_id)) in self
+            .src_start_ids
+            .iter()
+            .zip(self.src_end_ids.clone())
+            .enumerate()
+        {
+            if id >= *start_id && id <= end_id {
+                let offset = id - *start_id;
+                return self.dst_start_ids[index] + offset;
+            }
+        }
+        // If not mapped, return same
+        return id;
+    }
+}
+
+// I would really like the type system to enforce this, but I can't think of a way that isn't super painful.
+#[derive(PartialEq, Debug, Clone)]
+struct Almanac {
+    to_plant: Vec<IdNum>,
+    seed_to_soil: RangeMap,
+    soil_to_fertilizer: RangeMap,
+    fertilizer_to_water: RangeMap,
+    water_to_light: RangeMap,
+    light_to_temperature: RangeMap,
+    temperature_to_humidity: RangeMap,
+    humidity_to_location: RangeMap,
+}
+
+fn parse_almanac(string_list: &Vec<Vec<String>>) -> Almanac {
+    let mut to_plant: Vec<IdNum> = vec![];
+    let mut seed_to_soil: Option<RangeMap> = None;
+    let mut soil_to_fertilizer: Option<RangeMap> = None;
+    let mut fertilizer_to_water: Option<RangeMap> = None;
+    let mut water_to_light: Option<RangeMap> = None;
+    let mut light_to_temperature: Option<RangeMap> = None;
+    let mut temperature_to_humidity: Option<RangeMap> = None;
+    let mut humidity_to_location: Option<RangeMap> = None;
+    for grouping in string_list {
+        if grouping.len() == 0 {
+            // Ignore anything empty, probably a loading issue
+            continue;
+        }
+        let first_line = grouping.first().unwrap();
+        if first_line.ends_with(":") {
+            // We are in a section
+            let mut src_ids: Vec<IdNum> = vec![];
+            let mut dst_ids: Vec<IdNum> = vec![];
+            let mut ranges: Vec<IdNum> = vec![];
+
+            // get all the numbers first.
+            for line in grouping {
+                if line == first_line {
+                    continue;
+                }
+                let (dst, rest) = line.split_once(" ").unwrap();
+                let (src, range) = rest.split_once(" ").unwrap();
+                let dst_num: IdNum = dst.parse().unwrap();
+                let src_num: IdNum = src.parse().unwrap();
+                let range_num: IdNum = range.parse().unwrap();
+                src_ids.push(src_num);
+                dst_ids.push(dst_num);
+                ranges.push(range_num);
+            }
+
+            // Now determine case
+            if first_line.starts_with("seed") {
+                seed_to_soil = Some(RangeMap::new(
+                    src_ids,
+                    dst_ids,
+                    ranges,
+                    AlmanacItem::Seed,
+                    AlmanacItem::Soil,
+                ));
+            } else if first_line.starts_with("soil") {
+                soil_to_fertilizer = Some(RangeMap::new(
+                    src_ids,
+                    dst_ids,
+                    ranges,
+                    AlmanacItem::Soil,
+                    AlmanacItem::Fertilizer,
+                ));
+            } else if first_line.starts_with("fertilizer") {
+                fertilizer_to_water = Some(RangeMap::new(
+                    src_ids,
+                    dst_ids,
+                    ranges,
+                    AlmanacItem::Fertilizer,
+                    AlmanacItem::Water,
+                ));
+            } else if first_line.starts_with("water") {
+                water_to_light = Some(RangeMap::new(
+                    src_ids,
+                    dst_ids,
+                    ranges,
+                    AlmanacItem::Water,
+                    AlmanacItem::Light,
+                ));
+            } else if first_line.starts_with("light") {
+                light_to_temperature = Some(RangeMap::new(
+                    src_ids,
+                    dst_ids,
+                    ranges,
+                    AlmanacItem::Light,
+                    AlmanacItem::Temperature,
+                ));
+            } else if first_line.starts_with("temperature") {
+                temperature_to_humidity = Some(RangeMap::new(
+                    src_ids,
+                    dst_ids,
+                    ranges,
+                    AlmanacItem::Temperature,
+                    AlmanacItem::Humidity,
+                ));
+            } else if first_line.starts_with("humidity") {
+                humidity_to_location = Some(RangeMap::new(
+                    src_ids,
+                    dst_ids,
+                    ranges,
+                    AlmanacItem::Humidity,
+                    AlmanacItem::Location,
+                ));
+            }
+        } else {
+            // We are initial seeds
+            let (_, seed_nums) = first_line.split_once("seeds: ").unwrap();
+            for num in seed_nums.split(" ") {
+                if num.len() == 0 {
+                    continue;
+                }
+                let as_num: IdNum = num.parse().unwrap();
+                to_plant.push(as_num);
+            }
+        }
+    }
+
+    return Almanac {
+        to_plant: to_plant,
+        seed_to_soil: seed_to_soil.unwrap(),
+        soil_to_fertilizer: soil_to_fertilizer.unwrap(),
+        fertilizer_to_water: fertilizer_to_water.unwrap(),
+        water_to_light: water_to_light.unwrap(),
+        light_to_temperature: light_to_temperature.unwrap(),
+        temperature_to_humidity: temperature_to_humidity.unwrap(),
+        humidity_to_location: humidity_to_location.unwrap(),
+    };
+}
+
+fn almanac_to_locations(alm: &Almanac) -> Vec<IdNum> {
+    let mut result = vec![];
+    for seed in alm.to_plant.iter() {
+        let soil = alm.seed_to_soil.convert_id(*seed);
+        let fertilizer = alm.soil_to_fertilizer.convert_id(soil);
+        let water = alm.fertilizer_to_water.convert_id(fertilizer);
+        let light = alm.water_to_light.convert_id(water);
+        let temp = alm.light_to_temperature.convert_id(light);
+        let humidity = alm.temperature_to_humidity.convert_id(temp);
+        let location = alm.humidity_to_location.convert_id(humidity);
+        result.push(location);
+    }
+    return result;
+}
+
+/// Get lowest number location from this parsed nonsense.
+/// ```
+/// let input = day05::example_input();
+/// assert_eq!(day05::puzzle_a(&input), 35);
+/// ```
+pub fn puzzle_a(string_list: &Vec<Vec<String>>) -> IdNum {
+    let alm = parse_almanac(string_list);
+    let locations = almanac_to_locations(&alm);
+    return *locations.iter().min().unwrap();
+}
+
+/// Foo
+/// ```
+/// let input = day05::example_input();
+/// assert_eq!(day05::puzzle_b(&input), 0);
+/// ```
+pub fn puzzle_b(string_list: &Vec<Vec<String>>) -> IdNum {
+    return 0;
 }
--- day05/src/main.rs
+++ day05/src/main.rs
@@ -1,14 +1,16 @@
-use day05::load_no_blanks;
+use day05::load;
 use day05::puzzle_a;
 use day05::puzzle_b;
+use day05::split_lines_by_blanks;
 
 fn main() {
     let filename = "input";
-    let lines = load_no_blanks(filename);
+    let template = load(filename);
+    let groups = split_lines_by_blanks(&template);
 
-    let value = puzzle_a(&lines);
+    let value = puzzle_a(&groups);
     println!("Answer to 1st question: {}", value);
 
-    let value_b = puzzle_b(&lines);
+    let value_b = puzzle_b(&groups);
     println!("Answer to 2nd question: {}", value_b);
 }
