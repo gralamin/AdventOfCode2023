71b914b10e4d265bb626d8d13a8750c72831052f
Glen Nelson <glen.nelson.1@gmail.com>
2023-12-07 00:04:49 -0700

    day07: Solution for part 2
    The complex part here was making sure I had all the joker cases, which
    I handled via coverage and adding cases.

--- day07/src/lib.rs
+++ day07/src/lib.rs
@@ -20,14 +20,19 @@ enum CamelCard {
     Four,
     Three,
     Two,
+    Joker,
 }
 
-fn card_from_char(c: char) -> CamelCard {
-    match c {
+fn card_from_char(c: char, with_joker: bool) -> CamelCard {
+    let mut j_card = CamelCard::Jack;
+    if with_joker {
+        j_card = CamelCard::Joker;
+    }
+    return match c {
         'A' => CamelCard::Ace,
         'K' => CamelCard::King,
         'Q' => CamelCard::Queen,
-        'J' => CamelCard::Jack,
+        'J' => j_card,
         'T' => CamelCard::Ten,
         '9' => CamelCard::Nine,
         '8' => CamelCard::Eight,
@@ -38,7 +43,7 @@ fn card_from_char(c: char) -> CamelCard {
         '3' => CamelCard::Three,
         '2' => CamelCard::Two,
         _ => panic!("Bad character passed in"),
-    }
+    };
 }
 
 // Sorts such that Five of a kind comes first
@@ -55,40 +60,79 @@ enum HandType {
 
 fn find_hand_type(cards: &Vec<CamelCard>) -> HandType {
     let mut seen: HashMap<CamelCard, u8> = HashMap::new();
+    let mut jokers = 0;
     for card in cards {
+        if *card == CamelCard::Joker {
+            jokers += 1;
+            continue;
+        }
         *seen.entry(*card).or_insert(0_u8) += 1_u8;
     }
+    if jokers == 0 {
+        if seen.len() == 1 {
+            // Must be five of a kind
+            return HandType::FiveOfKind;
+        }
+        if seen.len() == 2 {
+            // Either four of a kind, or full house.
+            let first_value = seen.values().nth(0).unwrap();
+            if *first_value == 1_u8 || *first_value == 4_u8 {
+                return HandType::FourOfKind;
+            }
+            return HandType::FullHouse;
+        }
+        if seen.len() == 3 {
+            // Two pair, three of a kind
+            // we can tell by looking for a value of three
+            if seen
+                .values()
+                .filter(|&x| *x == 3_u8)
+                .collect::<Vec<_>>()
+                .len()
+                == 1
+            {
+                return HandType::ThreeOfKind;
+            }
+            return HandType::TwoPair;
+        }
+        if seen.len() == 4 {
+            // One pair
+            return HandType::OnePair;
+        }
+        return HandType::HighCard;
+    }
+
+    // With jokers logic
     if seen.len() == 1 {
-        // Must be five of a kind
+        // all jokers just add to five of a kind.
         return HandType::FiveOfKind;
     }
     if seen.len() == 2 {
-        // Either four of a kind, or full house.
+        // Four scenarios:
+        // JJJAB -> Four of kind
+        // JJAAB -> Four of kind
+        // JAABB -> FullHouse
+        // JAAAB -> Four of kind
+        // This becomes four of a kind unless both values equal and equal to 2.
         let first_value = seen.values().nth(0).unwrap();
-        if *first_value == 1_u8 || *first_value == 4_u8 {
-            return HandType::FourOfKind;
+        let second_value = seen.values().nth(1).unwrap();
+        if *first_value == *second_value && *first_value == 2_u8 {
+            return HandType::FullHouse;
         }
-        return HandType::FullHouse;
+        return HandType::FourOfKind;
     }
     if seen.len() == 3 {
-        // Two pair, three of a kind
-        // we can tell by looking for a value of three
-        if seen
-            .values()
-            .filter(|&x| *x == 3_u8)
-            .collect::<Vec<_>>()
-            .len()
-            == 1
-        {
-            return HandType::ThreeOfKind;
-        }
-        return HandType::TwoPair;
+        // JJABC -> Three of a kind, since its better than two pair.
+        // JAABC -> Three of a kind
+        // Just get highest count.
+        return HandType::ThreeOfKind;
     }
     if seen.len() == 4 {
-        // One pair
+        // JABCD -> One pair
         return HandType::OnePair;
     }
-    return HandType::HighCard;
+    // Must be five jokers
+    return HandType::FiveOfKind;
 }
 
 #[derive(Debug, Clone, PartialEq, Eq)]
@@ -110,12 +154,15 @@ impl PartialOrd for Hand {
     }
 }
 
-fn parse_hands(string_list: &Vec<String>) -> Vec<Hand> {
+fn parse_hands(string_list: &Vec<String>, joker: bool) -> Vec<Hand> {
     let mut result: Vec<Hand> = vec![];
     for line in string_list {
         let (cards_to_parse, value) = line.split_once(" ").unwrap();
         let bid: u32 = value.trim().parse().unwrap();
-        let cards: Vec<CamelCard> = cards_to_parse.chars().map(|c| card_from_char(c)).collect();
+        let cards: Vec<CamelCard> = cards_to_parse
+            .chars()
+            .map(|c| card_from_char(c, joker))
+            .collect();
         let hand_type = find_hand_type(&cards);
         let hand = Hand {
             bid: bid,
@@ -143,7 +190,7 @@ fn usize_to_u32(i: usize) -> u32 {
 /// assert_eq!(day07::puzzle_a(&vec1), 6440);
 /// ```
 pub fn puzzle_a(string_list: &Vec<String>) -> u32 {
-    let mut hands = parse_hands(string_list);
+    let mut hands = parse_hands(string_list, false);
     hands.sort();
     return hands
         .iter()
@@ -153,15 +200,28 @@ pub fn puzzle_a(string_list: &Vec<String>) -> u32 {
         .sum();
 }
 
-/// Foo
+/// As 1, but parse as jokers instead.
 /// ```
 /// let vec1: Vec<String> = vec![
-///     "foo"
+///     "32T3K 765",
+///     "T55J5 684",
+///     "KK677 28",
+///     "KTJJT 220",
+///     "QQQJA 483"
 /// ].iter().map(|s| s.to_string()).collect();
-/// assert_eq!(day07::puzzle_b(&vec1), 0);
+/// assert_eq!(day07::puzzle_b(&vec1), 5905);
 /// ```
 pub fn puzzle_b(string_list: &Vec<String>) -> u32 {
-    return 0;
+    let mut hands = parse_hands(string_list, true);
+    hands.sort();
+    println!("{:#?}", hands);
+    return hands
+        .iter()
+        .rev()
+        .enumerate()
+        .map(|(index, hand)| usize_to_u32(index + 1) * hand.bid)
+        .sum();
+    // Answer should be 243101568
 }
 
 #[cfg(test)]
@@ -171,10 +231,162 @@ mod tests {
     #[test]
     fn tst_parse() {
         let vec1: Vec<String> = vec!["QQQJA 483"].iter().map(|s| s.to_string()).collect();
-        let hands = parse_hands(&vec1);
+        let hands = parse_hands(&vec1, false);
         let hand = hands.first().unwrap();
         assert_eq!(hand.hand_type, HandType::ThreeOfKind);
         assert_eq!(hand.bid, 483);
+    }
+
+    #[test]
+    fn tst_parse_joker() {
+        let vec1: Vec<String> = vec!["QQQJA 483"].iter().map(|s| s.to_string()).collect();
+        let hands = parse_hands(&vec1, true);
+        let hand = hands.first().unwrap();
+        assert_eq!(hand.hand_type, HandType::FourOfKind);
+        assert_eq!(hand.bid, 483);
+    }
+
+    #[test]
+    fn tst_card_type_four_joker() {
+        let hand = vec![
+            CamelCard::Three,
+            CamelCard::Joker,
+            CamelCard::Joker,
+            CamelCard::Joker,
+            CamelCard::Joker,
+        ];
+        assert_eq!(find_hand_type(&hand), HandType::FiveOfKind);
+    }
+
+    #[test]
+    fn tst_card_type_three_joker() {
+        let hand = vec![
+            CamelCard::Three,
+            CamelCard::Two,
+            CamelCard::Joker,
+            CamelCard::Joker,
+            CamelCard::Joker,
+        ];
+        assert_eq!(find_hand_type(&hand), HandType::FourOfKind);
+    }
+
+    fn tst_card_type_three_joker_to_five() {
+        let hand = vec![
+            CamelCard::Three,
+            CamelCard::Three,
+            CamelCard::Joker,
+            CamelCard::Joker,
+            CamelCard::Joker,
+        ];
+        assert_eq!(find_hand_type(&hand), HandType::FiveOfKind);
+    }
+
+    #[test]
+    fn tst_card_type_two_joker_to_four() {
+        let hand = vec![
+            CamelCard::Three,
+            CamelCard::Two,
+            CamelCard::Three,
+            CamelCard::Joker,
+            CamelCard::Joker,
+        ];
+        assert_eq!(find_hand_type(&hand), HandType::FourOfKind);
+    }
+
+    #[test]
+    fn tst_card_type_two_joker_to_three() {
+        let hand = vec![
+            CamelCard::Three,
+            CamelCard::Two,
+            CamelCard::Four,
+            CamelCard::Joker,
+            CamelCard::Joker,
+        ];
+        assert_eq!(find_hand_type(&hand), HandType::ThreeOfKind);
+    }
+
+    #[test]
+    fn tst_card_type_one_joker_to_full() {
+        let hand = vec![
+            CamelCard::Three,
+            CamelCard::Two,
+            CamelCard::Three,
+            CamelCard::Two,
+            CamelCard::Joker,
+        ];
+        assert_eq!(find_hand_type(&hand), HandType::FullHouse);
+    }
+
+    #[test]
+    fn tst_card_type_one_joker_to_pair() {
+        let hand = vec![
+            CamelCard::Three,
+            CamelCard::Two,
+            CamelCard::Four,
+            CamelCard::Five,
+            CamelCard::Joker,
+        ];
+        assert_eq!(find_hand_type(&hand), HandType::OnePair);
+    }
+
+    #[test]
+    fn tst_card_type_high_card() {
+        let hand = vec![
+            CamelCard::Three,
+            CamelCard::Two,
+            CamelCard::Four,
+            CamelCard::Five,
+            CamelCard::Nine,
+        ];
+        assert_eq!(find_hand_type(&hand), HandType::HighCard);
+    }
+
+    #[test]
+    fn tst_card_type_plain_four() {
+        let hand = vec![
+            CamelCard::Three,
+            CamelCard::Three,
+            CamelCard::Three,
+            CamelCard::Three,
+            CamelCard::Nine,
+        ];
+        assert_eq!(find_hand_type(&hand), HandType::FourOfKind);
+    }
+
+    #[test]
+    fn tst_card_type_plain_five() {
+        let hand = vec![
+            CamelCard::Three,
+            CamelCard::Three,
+            CamelCard::Three,
+            CamelCard::Three,
+            CamelCard::Three,
+        ];
+        assert_eq!(find_hand_type(&hand), HandType::FiveOfKind);
+    }
+
+    #[test]
+    fn tst_card_type_fullhouse() {
+        let hand = vec![
+            CamelCard::Three,
+            CamelCard::Three,
+            CamelCard::Three,
+            CamelCard::Two,
+            CamelCard::Two,
+        ];
+        assert_eq!(find_hand_type(&hand), HandType::FullHouse);
+    }
+
+    #[test]
+    fn tst_card_type_five_jokers() {
+        let hand = vec![
+            CamelCard::Joker,
+            CamelCard::Joker,
+            CamelCard::Joker,
+            CamelCard::Joker,
+            CamelCard::Joker,
+        ];
+        assert_eq!(find_hand_type(&hand), HandType::FiveOfKind);
     }
 
     #[test]
@@ -251,5 +463,94 @@ mod tests {
             hands,
             vec![hand_five, hand_two, hand_three, hand_four, hand_one]
         )
+    }
+
+    #[test]
+    fn tst_sort_joker() {
+        let hand_one = Hand {
+            bid: 765,
+            cards: vec![
+                CamelCard::Three,
+                CamelCard::Two,
+                CamelCard::Ten,
+                CamelCard::Three,
+                CamelCard::King,
+            ],
+            hand_type: HandType::OnePair,
+        };
+
+        let hand_two = Hand {
+            bid: 684,
+            cards: vec![
+                CamelCard::Ten,
+                CamelCard::Five,
+                CamelCard::Five,
+                CamelCard::Joker,
+                CamelCard::Five,
+            ],
+            hand_type: HandType::FourOfKind,
+        };
+
+        let hand_three = Hand {
+            bid: 28,
+            cards: vec![
+                CamelCard::King,
+                CamelCard::King,
+                CamelCard::Six,
+                CamelCard::Seven,
+                CamelCard::Seven,
+            ],
+            hand_type: HandType::TwoPair,
+        };
+
+        let hand_four = Hand {
+            bid: 220,
+            cards: vec![
+                CamelCard::King,
+                CamelCard::Ten,
+                CamelCard::Joker,
+                CamelCard::Joker,
+                CamelCard::Ten,
+            ],
+            hand_type: HandType::FourOfKind,
+        };
+
+        let hand_five = Hand {
+            bid: 483,
+            cards: vec![
+                CamelCard::Queen,
+                CamelCard::Queen,
+                CamelCard::Queen,
+                CamelCard::Joker,
+                CamelCard::Ace,
+            ],
+            hand_type: HandType::FourOfKind,
+        };
+
+        let mut hands = vec![
+            hand_one.clone(),
+            hand_two.clone(),
+            hand_three.clone(),
+            hand_four.clone(),
+            hand_five.clone(),
+        ];
+        hands.sort();
+        assert_eq!(
+            hands,
+            vec![hand_four, hand_five, hand_two, hand_three, hand_one]
+        )
+    }
+
+    #[test]
+    fn additional_char_scenarios() {
+        assert_eq!(CamelCard::Nine, card_from_char('9', false));
+        assert_eq!(CamelCard::Four, card_from_char('4', false));
+        assert_eq!(CamelCard::Eight, card_from_char('8', false));
+    }
+
+    #[test]
+    #[should_panic]
+    fn invalid_character_in_parsing() {
+        card_from_char('z', false);
     }
 }
