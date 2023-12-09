bffa186bd24359b3f1d34aa8bc162dce4647c0eb
Glen Nelson <glen.nelson.1@gmail.com>
2023-12-08 23:08:34 -0700

    day09: Part 2 solution
    This is a simple recursive solution, just change where its being done.

--- day09/src/lib.rs
+++ day09/src/lib.rs
@@ -18,7 +18,13 @@ pub fn parse_reports(string_list: &Vec<String>) -> Vec<Vec<InputNum>> {
 }
 
 pub fn predict_next(history: &Vec<InputNum>) -> InputNum {
-    if history.iter().sum::<InputNum>() == 0 {
+    if history
+        .iter()
+        .filter(|&x| *x != 0)
+        .collect::<Vec<_>>()
+        .len()
+        == 0
+    {
         return 0;
     }
     let mut derivative_history = vec![];
@@ -47,13 +53,69 @@ pub fn puzzle_a(string_list: &Vec<String>) -> InputNum {
     return histories.iter().map(|x| predict_next(x)).sum();
 }
 
-/// Foo
+pub fn predict_first(history: &Vec<InputNum>) -> InputNum {
+    if history
+        .iter()
+        .filter(|&x| *x != 0)
+        .collect::<Vec<_>>()
+        .len()
+        == 0
+    {
+        return 0;
+    }
+    let mut derivative_history = vec![];
+    let mut first: Option<InputNum> = None;
+    for w in history.windows(2) {
+        let a = w[0];
+        let b = w[1];
+        let change = b - a;
+        derivative_history.push(change);
+        if let Some(_) = first {
+            continue;
+        }
+        first = Some(a);
+    }
+    // The result of the recursive call is the amount to change the first value by
+    let result = first.unwrap() - predict_first(&derivative_history);
+    return result;
+}
+
+/// Predict the first values.
 /// ```
 /// let vec1: Vec<String> = vec![
-///     "foo"
+///     "0 3 6 9 12 15",
+///     "1 3 6 10 15 21",
+///     "10 13 16 21 30 45"
 /// ].iter().map(|s| s.to_string()).collect();
-/// assert_eq!(day09::puzzle_b(&vec1), 0);
+/// assert_eq!(day09::puzzle_b(&vec1), 2);
 /// ```
 pub fn puzzle_b(string_list: &Vec<String>) -> InputNum {
-    return 0;
+    let histories = parse_reports(string_list);
+    return histories.iter().map(|x| predict_first(x)).sum();
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn tst_predict_first_first_sample() {
+        let vec1: Vec<InputNum> = vec![0, 3, 6, 9, 12, 15];
+        let result = predict_first(&vec1);
+        assert_eq!(result, -3);
+    }
+
+    #[test]
+    fn tst_predict_first_second_sample() {
+        let vec1: Vec<InputNum> = vec![1, 3, 6, 10, 15, 21];
+        let result = predict_first(&vec1);
+        assert_eq!(result, 0);
+    }
+
+    #[test]
+    fn tst_predict_first_third_sample() {
+        let vec1: Vec<InputNum> = vec![10, 13, 16, 21, 30, 45];
+        let result = predict_first(&vec1);
+        assert_eq!(result, 5);
+    }
 }
