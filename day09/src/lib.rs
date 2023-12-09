ec6a86d4527ef8582acc7d2138224deb582ef257
Glen Nelson <glen.nelson.1@gmail.com>
2023-12-08 22:50:16 -0700

    day09: Solution for part 1.
    A simple recursive way of doing a derivative here.

--- day09/src/lib.rs
+++ day09/src/lib.rs
@@ -2,15 +2,49 @@ extern crate filelib;
 
 pub use filelib::load_no_blanks;
 
-/// Foo
+type InputNum = i32;
+
+pub fn parse_reports(string_list: &Vec<String>) -> Vec<Vec<InputNum>> {
+    let mut result = vec![];
+    for line in string_list {
+        let mut cur_vec = vec![];
+        for n in line.split(" ") {
+            let n_u: InputNum = n.parse().unwrap();
+            cur_vec.push(n_u);
+        }
+        result.push(cur_vec);
+    }
+    return result;
+}
+
+pub fn predict_next(history: &Vec<InputNum>) -> InputNum {
+    if history.iter().sum::<InputNum>() == 0 {
+        return 0;
+    }
+    let mut derivative_history = vec![];
+    let mut last = 0;
+    for w in history.windows(2) {
+        let a = w[0];
+        let b = w[1];
+        let change = b - a;
+        derivative_history.push(change);
+        last = b;
+    }
+    return last + predict_next(&derivative_history);
+}
+
+/// Predict the next values.
 /// ```
 /// let vec1: Vec<String> = vec![
-///     "foo"
+///     "0 3 6 9 12 15",
+///     "1 3 6 10 15 21",
+///     "10 13 16 21 30 45"
 /// ].iter().map(|s| s.to_string()).collect();
-/// assert_eq!(day09::puzzle_a(&vec1), 0);
+/// assert_eq!(day09::puzzle_a(&vec1), 114);
 /// ```
-pub fn puzzle_a(string_list: &Vec<String>) -> u32 {
-    return 0;
+pub fn puzzle_a(string_list: &Vec<String>) -> InputNum {
+    let histories = parse_reports(string_list);
+    return histories.iter().map(|x| predict_next(x)).sum();
 }
 
 /// Foo
@@ -20,21 +54,6 @@ pub fn puzzle_a(string_list: &Vec<String>) -> u32 {
 /// ].iter().map(|s| s.to_string()).collect();
 /// assert_eq!(day09::puzzle_b(&vec1), 0);
 /// ```
-pub fn puzzle_b(string_list: &Vec<String>) -> u32 {
+pub fn puzzle_b(string_list: &Vec<String>) -> InputNum {
     return 0;
-}
-
-/// Delete this after starting on puzzle_a.
-/// ```
-/// let vec1: Vec<u32> = vec![];
-/// let vec2: Vec<u32> = vec![1];
-/// assert_eq!(day09::coverage_workaround(&vec1), 1);
-/// assert_eq!(day09::coverage_workaround(&vec2), 2);
-/// ```
-pub fn coverage_workaround(a: &Vec<u32>) -> u32 {
-    if a.len() == 0 {
-        return 1;
-    } else {
-        return 2;
-    }
 }
