093aa99f11986fbd155f6cb3f35bd0f35922a153
Glen Nelson <glen.nelson.1@gmail.com>
2023-12-18 23:54:01 -0700

    day19: Add solution for part b
    This is interval math again. Need a better way to do this, its very repeative
    since I'm bad at it right now.

--- day19/src/lib.rs
+++ day19/src/lib.rs
@@ -3,7 +3,7 @@ extern crate filelib;
 pub use filelib::load;
 pub use filelib::split_lines_by_blanks;
 
-type WorkflowValue = u32;
+type WorkflowValue = u64;
 
 #[derive(Debug, Copy, Clone, Eq, PartialEq)]
 enum Category {
@@ -248,7 +248,251 @@ pub fn puzzle_a(input: &Vec<Vec<String>>) -> WorkflowValue {
         .sum();
 }
 
-/// Foo
+#[derive(Debug, Copy, Clone, Eq, PartialEq)]
+struct PartRange {
+    x_mins: WorkflowValue,
+    x_maxs: WorkflowValue,
+    m_mins: WorkflowValue,
+    m_maxs: WorkflowValue,
+    a_mins: WorkflowValue,
+    a_maxs: WorkflowValue,
+    s_mins: WorkflowValue,
+    s_maxs: WorkflowValue,
+}
+
+fn recurse_derive_num_acceptable_parts(
+    flows: &Vec<Workflow>,
+    first_endpoint_name: &str,
+    in_range: PartRange,
+) -> Vec<PartRange> {
+    // Split mins / maxs by Flow
+    // Reject: Eliminate, don't add
+    // Accept: Take out, and add on to the recurse value at the end.
+    // SendTo: Recurse down the flow for each distinct send to.
+    let first: Vec<&Workflow> = flows
+        .iter()
+        .filter(|f| f.name == first_endpoint_name)
+        .collect();
+    let workflow = first[0];
+
+    let mut accept_ranges: Vec<PartRange> = vec![];
+    let mut range = in_range.clone();
+
+    for rule in workflow.rules.clone() {
+        // Check if range is impossible now, if it is, no point in continuing
+        if range.x_mins > range.x_maxs
+            || range.m_mins > range.m_maxs
+            || range.a_mins > range.a_maxs
+            || range.s_mins > range.s_maxs
+        {
+            break;
+        }
+
+        if let Some(cat) = rule.category {
+            if let Some(comp) = rule.compare {
+                if let Some(value) = rule.value {
+                    match rule.flow {
+                        Flow::Accept => {
+                            let mut to_accept = range.clone();
+                            match comp {
+                                Comparison::LessThan => {
+                                    // Add an accept that looks like range.
+                                    // Figure out value to alter by category
+                                    // we want to decrease max to be value - 1.
+                                    // and we want to change range to min value + 1
+                                    match cat {
+                                        Category::ExtremelyCoolLooking => {
+                                            to_accept.x_maxs = value - 1;
+                                            range.x_mins = value;
+                                        }
+                                        Category::Musical => {
+                                            to_accept.m_maxs = value - 1;
+                                            range.m_mins = value;
+                                        }
+                                        Category::Aerodynamic => {
+                                            to_accept.a_maxs = value - 1;
+                                            range.a_mins = value;
+                                        }
+                                        Category::Shiny => {
+                                            to_accept.s_maxs = value - 1;
+                                            range.s_mins = value;
+                                        }
+                                    }
+                                }
+                                Comparison::GreaterThan => {
+                                    // Add an accept that looks like range.
+                                    // Figure out value to alter by category
+                                    // we want to increase min to be value + 1.
+                                    // and we want to change range to max value - 1
+                                    match cat {
+                                        Category::ExtremelyCoolLooking => {
+                                            range.x_maxs = value;
+                                            to_accept.x_mins = value + 1;
+                                        }
+                                        Category::Musical => {
+                                            range.m_maxs = value;
+                                            to_accept.m_mins = value + 1;
+                                        }
+                                        Category::Aerodynamic => {
+                                            range.a_maxs = value;
+                                            to_accept.a_mins = value + 1;
+                                        }
+                                        Category::Shiny => {
+                                            range.s_maxs = value;
+                                            to_accept.s_mins = value + 1;
+                                        }
+                                    }
+                                }
+                            }
+                            accept_ranges.push(to_accept);
+                        }
+                        Flow::Reject => {
+                            match comp {
+                                Comparison::LessThan => {
+                                    // Alter self, make sure we aren't less than the value
+                                    // Set min to Value
+                                    match cat {
+                                        Category::ExtremelyCoolLooking => {
+                                            range.x_mins = value;
+                                        }
+                                        Category::Musical => {
+                                            range.m_mins = value;
+                                        }
+                                        Category::Aerodynamic => {
+                                            range.a_mins = value;
+                                        }
+                                        Category::Shiny => {
+                                            range.s_mins = value;
+                                        }
+                                    }
+                                }
+                                Comparison::GreaterThan => {
+                                    // Alter self, make sure we aren't more than the value
+                                    // Set max to Value
+                                    match cat {
+                                        Category::ExtremelyCoolLooking => {
+                                            range.x_maxs = value;
+                                        }
+                                        Category::Musical => {
+                                            range.m_maxs = value;
+                                        }
+                                        Category::Aerodynamic => {
+                                            range.a_maxs = value;
+                                        }
+                                        Category::Shiny => {
+                                            range.s_maxs = value;
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                        Flow::SendTo(c) => {
+                            let mut copy = range.clone();
+                            match comp {
+                                Comparison::LessThan => {
+                                    // This copy should get max = value - 1
+                                    // While range should get min = value
+                                    match cat {
+                                        Category::ExtremelyCoolLooking => {
+                                            copy.x_maxs = value - 1;
+                                            range.x_mins = value;
+                                        }
+                                        Category::Musical => {
+                                            copy.m_maxs = value - 1;
+                                            range.m_mins = value;
+                                        }
+                                        Category::Aerodynamic => {
+                                            copy.a_maxs = value - 1;
+                                            range.a_mins = value;
+                                        }
+                                        Category::Shiny => {
+                                            copy.s_maxs = value - 1;
+                                            range.s_mins = value;
+                                        }
+                                    }
+                                }
+                                Comparison::GreaterThan => {
+                                    // This copy should get min = value + 1
+                                    // While range should get max = value
+                                    match cat {
+                                        Category::ExtremelyCoolLooking => {
+                                            range.x_maxs = value;
+                                            copy.x_mins = value + 1;
+                                        }
+                                        Category::Musical => {
+                                            range.m_maxs = value;
+                                            copy.m_mins = value + 1;
+                                        }
+                                        Category::Aerodynamic => {
+                                            range.a_maxs = value;
+                                            copy.a_mins = value + 1;
+                                        }
+                                        Category::Shiny => {
+                                            range.s_maxs = value;
+                                            copy.s_mins = value + 1;
+                                        }
+                                    }
+                                }
+                            }
+                            let recurse = recurse_derive_num_acceptable_parts(flows, &c, copy);
+                            accept_ranges.extend(recurse);
+                        }
+                    }
+                }
+            }
+        } else {
+            match rule.flow {
+                Flow::Accept => {
+                    accept_ranges.push(range);
+                }
+                Flow::Reject => {
+                    // Throw away this entire range
+                }
+                Flow::SendTo(c) => {
+                    let recurse = recurse_derive_num_acceptable_parts(flows, &c, range);
+                    accept_ranges.extend(recurse);
+                }
+            }
+            break;
+        }
+    }
+
+    return accept_ranges;
+}
+
+fn derive_num_acceptable_parts(flows: &Vec<Workflow>, first_endpoint_name: &str) -> WorkflowValue {
+    let x_mins = 1;
+    let x_maxs = 4000;
+    let m_mins = 1;
+    let m_maxs = 4000;
+    let a_mins = 1;
+    let a_maxs = 4000;
+    let s_mins = 1;
+    let s_maxs = 4000;
+
+    let part_ranges = PartRange {
+        x_mins: x_mins,
+        x_maxs: x_maxs,
+        m_mins: m_mins,
+        m_maxs: m_maxs,
+        a_mins: a_mins,
+        a_maxs: a_maxs,
+        s_mins: s_mins,
+        s_maxs: s_maxs,
+    };
+    let ranges = recurse_derive_num_acceptable_parts(flows, first_endpoint_name, part_ranges);
+    return ranges
+        .iter()
+        .map(|s| {
+            (s.x_maxs - s.x_mins + 1)
+                * (s.m_maxs - s.m_mins + 1)
+                * (s.a_maxs - s.a_mins + 1)
+                * (s.s_maxs - s.s_mins + 1)
+        })
+        .sum();
+}
+
+/// Ignore parts, figure out how many combinations would be accepted.
 /// ```
 /// let vec1: Vec<Vec<String>> = vec![
 ///    vec!["px{a<2006:qkq,m>2090:A,rfg}",
@@ -268,8 +512,10 @@ pub fn puzzle_a(input: &Vec<Vec<String>>) -> WorkflowValue {
 ///    "{x=2461,m=1339,a=466,s=291}",
 ///    "{x=2127,m=1623,a=2188,s=1013}"].iter().map(|s| s.to_string()).collect()
 /// ];
-/// assert_eq!(day19::puzzle_b(&vec1), 0);
+/// assert_eq!(day19::puzzle_b(&vec1), 167409079868000);
 /// ```
 pub fn puzzle_b(input: &Vec<Vec<String>>) -> WorkflowValue {
-    return 0;
+    let first_endpoint_name = "in";
+    let (_, workflows) = parse_input(input);
+    return derive_num_acceptable_parts(&workflows, first_endpoint_name);
 }
